41a41e34f51cd43b085df0f149f7caab74ab0943
a.23308051280533 <a.23308051280533@cbtis128.edu.mx>
2026-02-11 20:33:33 +0000

    otra vez

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,103 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(AppMuebleria());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AppMuebleria extends StatelessWidget {
+  const AppMuebleria({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: LosVendedores(),
     );
   }
-}
+} // Fin clase AppMuebleria
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class LosVendedores extends StatelessWidget {
+  const LosVendedores({super.key});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
+ @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      // --- APPBAR ACTUALIZADO CON ICONO DE EMERGENCIA ---
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text(
+          "Muebleria Carrasco",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFF3E2723), // Café amaderado
+        centerTitle: true,
+        leading: const Icon(Icons.bed, color: Colors.white),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.build, color: Colors.white),
+            onPressed: () {},
+          ),
+          // Nuevo icono de emergencia añadido aquí
+          IconButton(
+            icon: const Icon(Icons.warning, color: Colors.white), 
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.camera_alt, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          _crearTarjetaMueble(
+            'https://raw.githubusercontent.com/Carrasco07/Imagenes-para-Flutter-6to-I-fecha-11-feb-2026/refs/heads/main/vendedor1.jpg',
+            'Elizabeth Galaviz',
+            'Empleada del Mes',
+          ),
+          _crearTarjetaMueble(
+            'https://raw.githubusercontent.com/Carrasco07/Imagenes-para-Flutter-6to-I-fecha-11-feb-2026/refs/heads/main/vendedor2.jpg',
+            'Arturo Carrasco',
+            'Vendedor',
+          ),
+          _crearTarjetaMueble(
+            'https://raw.githubusercontent.com/Carrasco07/Imagenes-para-Flutter-6to-I-fecha-11-feb-2026/refs/heads/main/vendedor3.jpg',
+            'Juan Perez',
+            'Aprendiz',
+          ),
+          _crearTarjetaMueble(
+            'https://raw.githubusercontent.com/Carrasco07/Imagenes-para-Flutter-6to-I-fecha-11-feb-2026/refs/heads/main/vendedor4.jpg',
+            'Rufino Torres',
+            'Vendedor',
+          ),
+        ],
+      ),
+    ); // Fin de Scaffold
+  }
+} // Fin clase LosVendedores
+
+Widget _crearTarjetaMueble(String urlImagen, String titulo, String subtitulo) {
+  return Card(
+    color: const Color(0xFFD7CCC8), // Café clarito
+    elevation: 6, // Sombreado
+    margin: const EdgeInsets.only(bottom: 16),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    child: ListTile(
+      leading: CircleAvatar(backgroundImage: NetworkImage(urlImagen)),
+      title: Text(
+        titulo,
+        style: const TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+      subtitle: Text(subtitulo, style: const TextStyle(color: Colors.black87)),
+      trailing: const Icon(
+        Icons.thumb_up,
+        color: Color(0xFF000080), // Azul Marino
+      ),
+      onTap: () {
+        // Acción al tocar la tarjeta
+      },
+    ),
+  );
 }
